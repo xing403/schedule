3167f8802e19c6ef2318b9b75b20eaf074399166
星如雨 <liyun200104@163.com>
2023-09-25 17:31:21 +0800

    🐞 fix: image load fail

--- src/pages/about.vue
+++ src/pages/about.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
+import path from 'path-browserify'
 
+const favicon_path = path.join('favicon.svg')
 </script>
 
 <template>
   <div flex="~ col gap-2" ma mt-15 w-500px text-center>
     <div>
-      <el-image bg-transparent src="/favicon.svg" :size="150" shape="square" />
+      <el-image bg-transparent :src="favicon_path" :size="150" shape="square" />
     </div>
     <el-divider border-style="dashed" />
     <div class="application" mt-5 text-4xl font-600>
