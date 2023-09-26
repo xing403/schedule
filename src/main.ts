61596f3f68fc1b0e972d88fbefc30505d83156f5
星如雨 <liyun200104@163.com>
2023-09-26 11:27:11 +0800

    🦄 refactor:  router

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,10 @@
 import { createApp } from 'vue'
-import { createRouter, createWebHashHistory } from 'vue-router'
 
 import ElementPlus from 'element-plus'
-import routes from 'virtual:generated-pages'
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 import App from './App.vue'
+
+import router from './router'
 
 import '@unocss/reset/tailwind.css'
 import 'uno.css'
@@ -13,10 +13,7 @@ import 'element-plus/theme-chalk/display.css'
 import 'element-plus/dist/index.css'
 
 const app = createApp(App)
-const router = createRouter({
-  history: createWebHashHistory(import.meta.env.BASE_URL),
-  routes,
-})
+
 for (const [key, component] of Object.entries(ElementPlusIconsVue))
   app.component(key, component)
 
